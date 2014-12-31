63fdc5e28cfb17fe1ec30a0917434265a1768711
wangxb110 <wangxb110@163.com>
2014-12-31 14:00:05 +0800

    Initial commit

---
+++ src/main/java/com/bobo/test/Test.java
@@ -0,0 +1,10 @@
+package com.bobo.test;
+
+/**
+ * Created by Administrator on 2014/12/31.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("dddd");
+    }
+}
